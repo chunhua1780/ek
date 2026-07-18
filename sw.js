668f454f24276f40b1f57e6a0c1af0c26fcd56fd
Jack <jack@ghostchat.app>
2026-07-18 14:13:44 +0000

    清理两个会话重复添加的人均时长统计(id冲突);SW升v14

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'b777-v13';
+const CACHE = 'b777-v14';
 const ASSETS = ['./', './index.html', './questions.js', './supabase.js', './manifest.json', './icon192.png', './icon512.png'];
 
 self.addEventListener('install', function(e){
