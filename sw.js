083f94d65852a37b61d84d02266e164e38aaf7f6
Jack <jack@ghostchat.app>
2026-07-18 14:20:09 +0000

    内部账号改为b777.员工号@gmail.com,绕过Supabase域名真实性校验;SW升v17

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'b777-v16';
+const CACHE = 'b777-v17';
 const ASSETS = ['./', './index.html', './questions.js', './supabase.js', './manifest.json', './icon192.png', './icon512.png'];
 
 self.addEventListener('install', function(e){
