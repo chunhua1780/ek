520b20b1270c84dabde0c92bae36c694084a0aa3
Jack <jack@ghostchat.app>
2026-07-22 10:17:40 +0000

    新增飞行计算器:时间/距离/速度计算、油耗计算、风修正;完全离线可用;SW升v28

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'b777-v27';
+const CACHE = 'b777-v28';
 const ASSETS = ['./', './index.html', './questions.js', './supabase.js', './manifest.json', './icon192.png', './icon512.png'];
 
 self.addEventListener('install', function(e){
