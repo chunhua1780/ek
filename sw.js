fb226790b5e9391c6480470086119d544a3d4a56
Jack <jack@ghostchat.app>
2026-07-22 11:15:44 +0000

    紧急修复:calcAll函数中tas未定义导致页面崩溃,全部替换为correctedSpeed;SW升v41

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'b777-v40';
+const CACHE = 'b777-v41';
 const ASSETS = ['./', './index.html', './questions.js', './supabase.js', './manifest.json', './icon192.png', './icon512.png'];
 
 self.addEventListener('install', function(e){
